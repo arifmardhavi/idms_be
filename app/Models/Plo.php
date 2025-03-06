b6076a720fb912198955d894bc1549e539fbe917
Arif Mardhavi <arifmardhavi@gmail.com>
2025-03-06 09:05:19 +0900

    add field duedays and rla_due_days

--- app/Models/Plo.php
+++ app/Models/Plo.php
@@ -9,7 +9,30 @@ class Plo extends Model
 {
     use HasFactory;
     protected $fillable = ['unit_id', 'no_certificate', 'issue_date', 'overdue_date', 'plo_certificate', 'plo_old_certificate', 'rla', 'rla_issue', 'rla_overdue', 'rla_certificate', 'rla_old_certificate'];
+    protected $appends = ['due_days', 'rla_due_days'];
 
+    public function getDueDaysAttribute()
+    {
+        return $this->calculateDaysDifference($this->overdue_date);
+    }
+
+    public function getRlaDueDaysAttribute()
+    {
+        return $this->calculateDaysDifference($this->rla_overdue);
+    }
+
+    private function calculateDaysDifference($date)
+    {
+        if (!$date) {
+            return null;
+        }
+
+        $targetTimestamp = strtotime($date);
+        $todayTimestamp = strtotime(now()->toDateString());
+
+        return ($targetTimestamp - $todayTimestamp) / 86400; // 86400 = jumlah detik dalam sehari
+    }
+    
     public function unit()
     {
         return $this->belongsTo(Unit::class);
