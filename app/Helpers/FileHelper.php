7da9b7075d5e8d5ee6cb9dbfd60ce9f612a1e909
M. Arif Mardhavi <arifmardhavi@gmail.com>
2026-01-24 11:44:15 +0900

    Add file upload with versioning and delete helper
    Introduces uploadWithVersion to handle file uploads with automatic versioning to prevent overwrites, and deleteFile to remove files from a specified folder. These utilities enhance file management capabilities in the FileHelper class.

--- app/Helpers/FileHelper.php
+++ app/Helpers/FileHelper.php
@@ -6,6 +6,34 @@ use Illuminate\Support\Facades\File;
 
 class FileHelper
 {
+
+    public static function uploadWithVersion($file, $destinationFolder){
+        $originalName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME); // Ambil nama file original tanpa ekstensi
+        $extension = $file->getClientOriginalExtension(); // Ambil ekstensi file
+        $dateNow = date('dmY'); // Tanggal sekarang dalam format ddmmyyyy
+        $version = 0; // Awal versi
+        // Format nama file
+        $filename = $originalName . '_' . $dateNow . '_' . $version . '.' . $extension;
+
+        // Cek apakah file dengan nama ini sudah ada di folder tujuan
+        while (file_exists(public_path($destinationFolder."/".$filename))) {
+            $version++;
+            $filename = $originalName . '_' . $dateNow . '_' . $version . '.' . $extension;
+        }
+        // Store file in public/contract_new
+        $file->move(public_path($destinationFolder), $filename);
+        return $filename;
+    }
+
+    public static function deleteFile($filename, $destinationFolder){
+        $remove_path = public_path($destinationFolder."/".$filename);
+        if (file_exists($remove_path)) {
+            unlink($remove_path); // Hapus file
+            return true; // Berhasil dihapus
+        }
+        return false; // File tidak ada atau gagal dihapus
+    }
+
     public static function folderSize($dir)
     {
         $size = 0;
